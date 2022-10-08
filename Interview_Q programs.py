24cd6e93fcde33ce6c227cd72636f4588a7e40c0
chsrikanth750 <115348006+chsrikanth750@users.noreply.github.com>
2022-10-08 15:58:18 +0530

    interview quation
    interview quations for automation testing

---
+++ Interview_Q programs.py
@@ -0,0 +1,151 @@
+1) Fibanacci Series
+1) Fibanacci Series
+2) Factoral for given number
+3) Remove duplicate values from list
+4) Repeated charecters count in string 
+5) How to get even numbers from range of 1 to 100
+6) How to get odd numbers from range of 1 to 100
+7) How to know given number is even or odd 
+8) How to list prime numbers in range of 1 to 20
+9) How to know given numbers is prime number or not
+10) Prime number in list comprihence
+11) Gmail id matching using regular expressions
+12) IP address matching using regular expressions
+import re
+ip = '1.200.33.25
+print(re.search(r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', ip).group())
+16) MAC address matching using regular expressions
+17) Reverse a string 
+18) Reverse words in a given string 'exp' => ' hi how are you' to 'ih woh era uoy'
+19) Sort a list with out inbuild functions
+20) Get max value from list with out inbuilt function 
+21) Anagram logic
+22) Palindrome Logic
+23) Swap 2 values of 2 variables without using a third variable
+24) Find Max and Min values without max and min functions
+25) Convert two lists into one dictionary and list into dictionary
+26) find Missing number in list 'ex' => [1,2,3,5,6] 4 is missing number 
+27) remove duplicate characters in string
+28) Get duplicate lines count in a file
+29) Get only number words count in a file
+30) Get repeated words count in a file
+31) Get each character count in a string 
+32) write one file data into another file 
+33) write 3files data into one file 
+34) Use of *args and **kwargs 
+35) How to find given string is lower or not ?
+36) D/B extend and append in list methods ?
+37) D/B remove and pop ?
+38) D/B list and tuple ?
+39) D/B open and with open to open a file ?
+40) D/B match and search methods in re module ?
+41) How to handle exceptions in python ?
+42) Use of break, pass and continue ?
+43) Use of type() and dir() functions ?
+44) Use of self in class method.
+45) Use of __init__ method in class ?
+46) Use of dir() function ?
+47) What is inheritance and D/B multi inheritance and multi-level ?
+48) Types of parameters for function ?
+49) Use of map() and reduce() functions ?
+50) Use of decorator ?
+51) difference between iterator and generator?
+52) How to convert list values into string ?
+53) How to match date using regx
+54) Use of re.sub ?
+55) Use of id() function.
+56) Use of update method in dictionary.
+57) Get a even numbers from 1 to 100 using map
+58) How to add new value to set ?
+59) how to know list length ?
+60) Use os super function ? 
+61) print the palindromic available partitions of a string with python
+
+
+            ####      ROBOT FRAMEWORK       ####
+1) What is robot framework.
+2) How to develop keywords 
+3) How to import python file 
+4) What are the predefine keywords you have used.
+5) How to define library 
+6) What is resouse file and how to import.
+7) How to run particular test case
+8) How to run failed jobs 
+9) Use of dryrun option 
+10) How to continue script if keyword fail
+11) Use of tags
+
+
+
+           ###      SELENIUM       ####
+1) What is selenium 
+2) Types of waits and difference
+3) How to get window size ?
+4) How to open new tab ?
+5) How to select dropdown values 
+6) How to click on element 
+7) How to take screenshot 
+8) ID and Xpath which one is fast
+9) How to get text 
+10) How to get window title
+11) Types of selenium exceptions 
+12) How to do mouse operations 
+13) How to do double click operations 
+14) How to handle popups 
+15) How to refresh page
+16) How to do scroll down
+17) How to enter text into text box
+18) What are the element identifiers
+
+
+                ###          REST API         ###
+1) What is web services 
+2) Difference between SOAP and REST
+3) Difference between post and put
+4) What are the response cods 
+5) What is the status of return code 200 and 201,202,400,404, 500
+6) Use of get method
+7) What are the HTTP methods
+8) How did you automated REST calls ?
+9) How did you test REST calls Manullay ?
+
+
+                ###          Testng         ###
+1. What is SDLE ?
+2. What is bug life cycle?
+3. What is regression testing ?
+4. What is sanity testing ?
+5. What is TDLC ?
+6. What is functional testing ?
+7. What is agile methodology?
+8) Difference between severity and priority ?
+9) severity levels ?
+10) priority levels ?
+11) What are testing lvels ?
+12) What are testing types ?
+13) Difference between Alpha testing and Beta testing ?
+14) Difference between Block box testing and White box testing ?
+15) Use of putty and Winscp tools ?
+16) How do you know system IP address ?
+17) Use of ping command ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
